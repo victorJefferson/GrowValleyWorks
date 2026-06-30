17ae99bf8e94d48057b01007890db2c38a32d926
vjsamuelideas2it <edwin.xavier@as-software.com>
2026-06-30 15:46:14 +0530

    Bump Sanity apiVersion to break fetch cache for UAE Golden Visa

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -7,7 +7,7 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const client = createClient({
     projectId,
     dataset,
-    apiVersion: "2024-07-02",
+    apiVersion: "2024-07-03",
     useCdn: false,
 });
 
