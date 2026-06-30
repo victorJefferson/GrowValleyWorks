5958fa847568b66c861a76b78238eebfce777d26
vjsamuelideas2it <edwin.xavier@as-software.com>
2026-06-30 15:38:08 +0530

    Bump Sanity apiVersion to break fetch cache for Saudi

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -7,7 +7,7 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const client = createClient({
     projectId,
     dataset,
-    apiVersion: "2024-06-30",
+    apiVersion: "2024-07-01",
     useCdn: false,
 });
 
